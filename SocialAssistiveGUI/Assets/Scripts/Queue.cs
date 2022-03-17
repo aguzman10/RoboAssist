5e00f38e1a4bbabd9276976a734339b8029a76f5
Colt Ford <cbf3j@mtmail.mtsu.edu>
2022-03-17 02:29:43 -0500

    Buttons, Prefabs, and Scripts
    Some basic button assets were added from the Unity Store and placed into the scene. Scripts and Prefabs were created for buttons and the Activity Queue. Currently, motion objects can be added to the queue using buttons. The play button just displays the queue contents in the console.

---
+++ SocialAssistiveGUI/Assets/Scripts/Queue.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Activity Queue", menuName = "ActivityQueues")]
+public class Queue : ScriptableObject
+{
+    public LinkedList<MotionObject> activityQueue = new LinkedList<MotionObject>();
+    public void AddMotion(MotionObject motion)
+    {
+        activityQueue.AddLast(motion);
+    }
+
+    public void PrintQueue()
+    {
+        foreach (var item in activityQueue)
+        {
+            Debug.Log(item.MotionType);
+        }
+    }
+}
