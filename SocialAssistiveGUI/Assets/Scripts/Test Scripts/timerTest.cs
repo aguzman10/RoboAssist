e57ae30687f4a0de387b7fd858bf6de5db816a3a
adfine <adf5f@mtmail.mtsu.edu>
2022-03-18 12:57:30 -0500

    Added Test Scripts folder and timerTest.cs
    Test Scripts folder created to separate test scripts from final scripts. timerTest.cs gives an example of how to get a timestamp in Unity and how to implement a timer that can be used for determining when to enter the Not Playing state

---
+++ SocialAssistiveGUI/Assets/Scripts/Test Scripts/timerTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class timerTest : MonoBehaviour
+{
+    private float timer = 0.0f;
+    public float waitTime = 30.0f;
+
+
+    private void Start()
+    {
+        print(System.DateTime.Now.ToString("HH:mm:ss:f")); // Example of how to get a timestamp
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime; // Time.deltaTime = seconds between last frame and current frame
+        print(timer);
+        if (timer > waitTime) // Reset timer when it exceeds the wait time
+            timer = 0.0f;
+    }
+}
