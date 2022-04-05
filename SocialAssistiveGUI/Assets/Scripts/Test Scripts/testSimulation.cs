e441b714a3a83952281da88efa5fce0224ebe320
adfine <adf5f@mtmail.mtsu.edu>
2022-04-05 11:50:40 -0500

    Fixed bug with DisplayMessage()
    Prevents unwanted calls to DisplayMessage()

--- SocialAssistiveGUI/Assets/Scripts/Test Scripts/testSimulation.cs
+++ SocialAssistiveGUI/Assets/Scripts/Test Scripts/testSimulation.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class testSimulation : MonoBehaviour
 {
     System.Random rnd = new System.Random(); // Namespace conflict between UnityEngine and System, specify System.Random
-
+    
     public Queue q;
 
     LinkedList<string> activityQueue = new LinkedList<string>(); // Linked list of movements user should perform
@@ -14,11 +14,12 @@ public class testSimulation : MonoBehaviour
     ActivityTimer timer = new ActivityTimer(); // Used to determine when to enter Not Playing State
 
     bool finished = false; // Determines if activity exercise is finished
+    bool simulationRunning = false; // Determines if start button on GUI has been pressed
+    bool notPlayingVisited = false; // To trigger Playing Poorly
     int consecWrong = 0; // Number of consecutively wrong movements made
     double chance; // Used to determine if simulated user should perform a movement or not
     string timestamp; // Timestamp that state is entered
     string message; // Message to be displayed
-    bool notPlayingVisited = false; // To trigger Playing Poorly
 
     public float waitTime = 2.0f; // Threshold to trigger Not Playing
     public string userName = "Misty"; // Name of simulated user
@@ -26,9 +27,13 @@ public class testSimulation : MonoBehaviour
     public int wellThreshold = 3; // Threshold to trigger Playing Well
     public int poorlyThreshold = 3; // Threshold to trigger Playing Poorly
     public int maxCycles = 5; // Maximum number of times the do while loop will execute (prevents infinite loop in when trying to test Not Playing State)
-
+    
     // Start is called before the first frame update
     // Start State
+    public void Start()
+    {
+        Time.timeScale = 0f;
+    }
     public void StartSimulation()
     {
         // Initialize linked list
@@ -39,25 +44,28 @@ public class testSimulation : MonoBehaviour
         user = new SimulatedUser(userName);
         user.targetMovement = node.Current;
 
-        GetTimeStamp();
+        //GetTimeStamp();
         timer.StartTimer();
-
-        
+        Time.timeScale = 1f;
+        simulationRunning = true;
+        message = "";
+        finished = false;
     }
 
     // Called once per frame
     private void Update()
     {
-        if (timer.IsRunning())
+        if (timer.IsRunning() && simulationRunning)
         {
             timer.elapsedTime += Time.deltaTime;
         }
     }
 
     // LateUpdate() called after Update() finishes
-    private void LateUpdate()
+    private void FixedUpdate()
     {
-        if (notPlayingVisited == false) // Ensure Not Playing is visited
+        
+        if (notPlayingVisited == false) // Ensure Not Playing is visited at least once for testing purposes
         {
             print("waiting");
             if (timer.elapsedTime > waitTime) 
@@ -85,7 +93,6 @@ public class testSimulation : MonoBehaviour
                 if (chance > notPlayingChance) // User should perform a movement
                 {
                     user.GenerateMovement(); // Perform a movement
-                    //timer.StopTimer(); // Stop timer -> May not need to stop the timer until activity is  completely finished (?)
                     timer.ResetTimer(); // Reset timer
 
                     if (user.chosenMovement == user.targetMovement) // User performed correct movement
@@ -99,7 +106,7 @@ public class testSimulation : MonoBehaviour
                         }
                         else // Correct Movement State
                         {
-                            GetTimeStamp();
+                           GetTimeStamp();
                             message = "Correct!";
                         }
                         if (!node.MoveNext()) // Move to next node in linked list
@@ -124,11 +131,15 @@ public class testSimulation : MonoBehaviour
             }
 
         }
-        if(!finished)
-            DisplayMessage(message); // Display Message State
+        if (!finished && simulationRunning && message != "") // Display State
+        {
+            GetTimeStamp();
+            DisplayMessage(); // Display Message State
+        }
 
         if (finished) // Finished State
         {
+            Time.timeScale = 0f;
             GetTimeStamp();
             timer.StopTimer();
             print("You finished your exercise activity! Good job!");
@@ -137,20 +148,22 @@ public class testSimulation : MonoBehaviour
 
             // Save user performance here
 
-            //UnityEditor.EditorApplication.isPlaying = false; // Terminates program execution in Unity Editor
+            UnityEditor.EditorApplication.isPlaying = false; // Terminates program execution in Unity Editor
         }
     }
 
+    
+
     // Grabs timestamp (should be called each time a state is entered)
-    void GetTimeStamp()
+    private void GetTimeStamp()
     {
         timestamp = DateTime.Now.ToString("HH:mm:ss:f"); // Hours:Minutes:Seconds:Tenths of a second
     }
 
-    // Display State
-    void DisplayMessage(string message) {
-        GetTimeStamp();
-        print(message);  
+    // Display message to debug console
+    private void DisplayMessage() {
+        print(message);
+        message = "";
     }
 
 
